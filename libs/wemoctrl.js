5c0e073b9e449d5cca2966da9afacd1ab516203a
Inho Oh <webispy@gmail.com>
2016-11-08 23:07:20 -0500

    Add wemo search retry

--- libs/wemoctrl.js
+++ libs/wemoctrl.js
@@ -3,16 +3,34 @@
 const settings = require('./settings')
 const ipc = require('./ipc_slave')
 const WeMo = require('wemo')
-const client = WeMo.Search()
 
+let client = null
 let handle = null
 
-client.on('found', (device) => {
-  console.log('wemo found:', device.ip, device.port)
-  handle = new WeMo(device.ip, device.port)
-  settings.data.wemo = device.ip
-  ipc.sendStatus()
-})
+function startSearch() {
+  if (client != null) {
+    client._stop()
+  }
+
+  client = WeMo.Search()
+  console.log('Start WeMo search')
+
+  client.on('found', (device) => {
+    console.log('wemo found:', device.ip, device.port)
+    handle = new WeMo(device.ip, device.port)
+    settings.data.wemo = device.ip
+    ipc.sendStatus()
+  })
+}
+
+startSearch()
+
+setInterval(function() {
+  if (handle == null) {
+    console.log('can't find WeMo in 5 secs. retry WeMo search')
+    startSearch()
+  }
+}, 5000)
 
 module.exports.getStatus = function (cb) {
   if (handle == null) {
